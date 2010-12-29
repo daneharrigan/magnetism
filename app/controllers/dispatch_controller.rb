846b7eac976c1935fe1afdcb40dbe9e3cd296614
Dane Harrigan <dane.harrigan@gmail.com>
2010-12-29 11:08:53 -0500

    dispatch_controller returns the default 404 page. can i make this dynamic?

--- app/controllers/dispatch_controller.rb
+++ app/controllers/dispatch_controller.rb
@@ -1,4 +1,6 @@
 class DispatchController < ApplicationController
+  layout nil
+
   def show
     page = Page.find_by_path(params[:path] || '')
     # params[:path] is nil when / is requested
@@ -8,6 +10,6 @@ class DispatchController < ApplicationController
     page.current!
     render :text => Liquify.render(page.template.content)
   rescue
-      render :text => 'Page not found', :status => 404
+    render :file => "#{Rails.root}/public/404.html", :status => 404
   end
 end
