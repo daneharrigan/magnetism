867f404e6c1cff5c789f5dd28f43c6db9a0b7a0e
Dane Harrigan <dane.harrigan@gmail.com>
2011-01-15 01:06:07 -0500

    refactored edit_permalink helper method

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -1,10 +1,10 @@
 module PageHelper
   def editable_permalink(page)
-    html = page.parent.try(:permalink) || ''
+    html = page.parent.try(:permalink) || ''.html_safe
 
     unless page.homepage?
-      html << '/'
       html = content_tag(:span, html, :id => 'permalink-prefix')
+      html << '/' unless page.parent.homepage?
     end
 
     html << content_tag(:span, page.slug, :id => 'permalink-slug')
