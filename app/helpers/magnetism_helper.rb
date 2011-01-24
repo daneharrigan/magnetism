550cb4be189d45a149d2eed3617013c6a6ec7890
Dane Harrigan <dane.harrigan@gmail.com>
2011-01-24 12:31:09 -0500

    moved methods from application helper to magnetism helper

--- app/helpers/magnetism_helper.rb
+++ app/helpers/magnetism_helper.rb
@@ -1,4 +1,14 @@
 module MagnetismHelper
+  def section_stylesheet_link_tag
+    full_path = "/admin/stylesheets/#{controller.controller_name}"
+    stylesheet_link_tag(full_path) if File.exists?("#{Rails.root}/public#{full_path}.css")
+  end
+
+  def section_javascript_include_tag
+    full_path = "/admin/javascripts/#{controller.controller_name}"
+    javascript_include_tag(full_path) if File.exists?("#{Rails.root}/public#{full_path}.js")
+  end
+
   def site_selector_collection
     @site_selector_collection ||= Site.all.map { |site| link_to site.name, admin_manage_site_path(site) }
   end
--- lib/magnetism.rb
+++ lib/magnetism.rb
@@ -12,3 +12,39 @@ require 'fog'
 require 'mini_magick'
 require 'carrierwave'
 
+# Magnetism.setup do |s|
+#   # write files to the local filesystem
+#   s.storage = :file
+#
+#   # keep files on s3
+#   s.storage = :s3
+    #
+    #     CarrierWave.configure do |config|
+    #       config.s3_access_key_id = "xxxxxx"
+    #       config.s3_secret_access_key = "xxxxxx"
+    #       config.s3_bucket = "my_bucket_name"
+    #       config.s3_cnamed = true
+    #       config.s3_bucket = 'bucketname.domain.tld'
+    #       config.s3_region = 'eu-west-1'
+
+#   s.storage :cloud_files
+    #     CarrierWave.configure do |config|
+    #       config.cloud_files_username = "xxxxxx"
+    #       config.cloud_files_api_key = "xxxxxx"
+    #       config.cloud_files_container = "my_container"
+    #       config.cloud_files_cdn_host = "c000000.cdn.rackspacecloud.com"
+#
+#   SIMILARITIES:
+#     api_key
+#     secret
+# end
+
+module Magnetism
+  def self.storage
+  end
+
+  def self.setup
+    yield(self)
+  end
+end
+
