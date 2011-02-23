b141806b56361c876465cb1def4840adf38ecb67
Dane Harrigan <dane.harrigan@gmail.com>
2011-02-22 22:13:20 -0500

    updated snippet_tag to reflect Liquify changes

--- app/tags/snippet_tag.rb
+++ app/tags/snippet_tag.rb
@@ -2,6 +2,6 @@ class SnippetTag < Liquify::Tag
   def invoke(params)
     snippets = Site.current.theme.templates.snippets
     template = snippets.first(:conditions => { :name => params.first })
-    Liquify.render(template.content)
+    Liquify.invoke(template.content)
   end
 end
