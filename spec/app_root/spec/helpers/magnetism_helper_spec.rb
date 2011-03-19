9715173c0502b40c667014917dfd6effbd24a881
Dane Harrigan <dane.harrigan@gmail.com>
2011-03-19 00:34:21 -0400

    refactored helpers and supporting specs

--- spec/app_root/spec/helpers/magnetism_helper_spec.rb
+++ spec/app_root/spec/helpers/magnetism_helper_spec.rb
@@ -4,9 +4,12 @@ describe MagnetismHelper do
   describe '#section_stylesheet_link_tag' do
     context 'when a file exists' do
       it 'returns the link tag to that file' do
-        File.stub(:exists? => true)
         name = 'controller-name'
         controller.stub(:controller_name => name)
+
+        glob = []
+        glob.stub :empty? => false
+        Dir.stub :glob => glob
 
         tag = %{<link href="/admin/stylesheets/#{name}.css" media="screen" rel="stylesheet" type="text/css" />}
         section_stylesheet_link_tag.should == tag
@@ -15,7 +18,10 @@ describe MagnetismHelper do
 
     context 'when the file does not exist' do
       it 'returns nothing' do
-        File.stub(:exists? => false)
+        glob = []
+        glob.stub :empty? => true 
+        Dir.stub :glob => glob
+
         section_stylesheet_link_tag.should be_nil
       end
     end
@@ -24,7 +30,10 @@ describe MagnetismHelper do
   describe '#section_javascript_include_tag' do
     context 'when a file exists' do
       it 'returns the script tag to that file' do
-        File.stub(:exists? => true)
+        glob = []
+        glob.stub :empty? => false
+        Dir.stub :glob => glob
+
         name = 'controller-name'
         controller.stub(:controller_name => name)
 
@@ -35,12 +44,14 @@ describe MagnetismHelper do
 
     context 'when the file does not exist' do
       it 'returns nothing' do
-        File.stub(:exists? => false)
+        glob = []
+        glob.stub :empty? => true
+        Dir.stub :glob => glob
+
         section_javascript_include_tag.should be_nil
       end
     end
   end
-
 
   describe '#site_selector_collection' do
     before(:each) { Factory(:site) }
