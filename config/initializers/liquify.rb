2a4ad4993cff7d317ff714fb09a804b790fcdcdf
Dane Harrigan <dane.harrigan@gmail.com>
2011-04-01 00:09:58 -0400

    added published scope to navigation

--- config/initializers/liquify.rb
+++ config/initializers/liquify.rb
@@ -1,6 +1,6 @@
 Liquify.setup do |config|
   config.register_drop :site, lambda { Site.current }
-  config.register_drop :navigation, lambda { Site.current.homepage.pages }
+  config.register_drop :navigation, lambda { Site.current.homepage.pages.published }
   config.register_drop :page, lambda { Page.current }
   config.register_drop :parent, lambda { Page.current.parent }
 
