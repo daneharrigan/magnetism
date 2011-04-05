906c974c6b21010ec0da3c3e98d85680864e3cc4
Dane Harrigan <dane.harrigan@gmail.com>
2011-04-05 10:56:24 -0400

    removed blog model

--- app/models/blog.rb
+++
@@ -1,8 +0,0 @@
-class Blog < ActiveRecord::Base
-  include Liquify::Methods
-
-  belongs_to :page
-  has_many :comments
-
-  liquify_methods :article, :excerpt
-end
--- spec/app_root/spec/models/blog_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe Blog do
-  it { should belong_to(:page) }
-  it { should have_many(:comments) }
-end
