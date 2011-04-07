11d689d3ce424424adf02bf061d18ae99ac41a5d
Dane Harrigan <dane.harrigan@gmail.com>
2011-04-07 00:21:05 -0400

    email is not required to comment

--- spec/app_root/spec/models/comment_spec.rb
+++ spec/app_root/spec/models/comment_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Comment do
   it { should belong_to(:page) }
   it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:email) }
   it { should validate_presence_of(:message) }
 
   describe '#gravatar' do
