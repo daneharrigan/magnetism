a5bad5257af207bf941de1fa14cc1e54c34f5cd5
Dane Harrigan <dane.harrigan@gmail.com>
2011-04-14 00:15:09 -0400

    consolidated public controller with dispatch

--- app/controllers/dispatch_controller.rb
+++ app/controllers/dispatch_controller.rb
@@ -1,5 +1,11 @@
-class DispatchController < PublicController
+require 'magnetism/page_not_found'
+
+class DispatchController < ApplicationController
   include Magnetism::Cache
+
+  layout nil
+  around_filter :page_not_found
+  after_filter :clear_current
 
   def show
     if current_page.comment?
@@ -9,4 +15,31 @@ class DispatchController < PublicController
       render :text => Liquify.invoke(current_page.template.content)
     end
   end
+
+  private
+    def current_page
+      return @page if @page
+
+      site = Site.find_by_request(request)
+      raise Magnetism::PageNotFound if site.nil?
+
+      site.current!
+      @page = site.pages.find_by_path(request)
+
+      raise Magnetism::PageNotFound if @page.nil?
+
+      @page.current!
+      return @page
+    end
+
+    def clear_current
+      Site.clear_current!
+      Page.clear_current!
+    end
+
+    def page_not_found
+      yield
+      rescue Magnetism::PageNotFound
+        render :nothing => true, :status => 404
+    end
 end
